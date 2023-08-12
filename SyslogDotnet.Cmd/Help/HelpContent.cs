221dc5ed18d1cee34e76dc92375bb5cae53b16f5
tgiqfe <tqji31001@gmail.com>
2023-08-12 12:00:55 +0900

    Update help.

--- SyslogDotnet.Cmd/Help/HelpContent.cs
+++ SyslogDotnet.Cmd/Help/HelpContent.cs
@@ -22,7 +22,7 @@ namespace SyslogDotnet.Cmd.Help
         public static string HelpBlock1()
         {
             //  Version
-            //    ---------1---------2---------3---------4---------5---------6---------7---------8---------9--------10
+            //  ---2---------3---------4---------5---------6---------7---------8---------9--------10
             var version = Assembly.GetExecutingAssembly().GetName().Version;
 
             return $"SyslogDotnet {version.Major}.{version.Minor}.{version.Build}.{version.Revision}" + "\r\n";
@@ -33,35 +33,39 @@ namespace SyslogDotnet.Cmd.Help
             //  Useage
             //  ---2---------3---------4---------5---------6---------7---------8---------9--------10
             return @"Useage:
-    syslogdotnet server [/u Address:Port] [/t Address:Port] [/l] [/r CertFile]
-                        [/w CertPassword] [/q] [/e PermittedPeer]
-    syslogdotnet client [/s URI] [/l] [/r CertFile] [/w CertPassword] [/i] [/t Timeout]
-                        [/f Format] [/c Facility] [/v Severity] [/m MEssage] [/a AppName]
-                        [/b Hostname] [/d ProcId] [/j MsgId] [/x StructuredData ... ]
-    syslogdotnet /?" + "\r\n2";
+  syslogdotnet server [/u Address:Port] [/t Address:Port] [/l] [/r CertFile]
+                      [/w CertPassword] [/q] [/e PermittedPeer]
+  syslogdotnet client [/s URI] [/l] [/r CertFile] [/w CertPassword] [/i] [/t Timeout]
+                      [/f Format] [/c Facility] [/v Severity] [/m MEssage] [/a AppName]
+                      [/b Hostname] [/d ProcId] [/j MsgId] [/x StructuredData ... ]
+  syslogdotnet /?
+";
         }
 
         public static string HelpBlock3()
         {
             //  Sub Command
-            //              ---------1---------2---------3---------4---------5---------6---------7---------8---------9--------10
+            //  ---2---------3---------4---------5---------6---------7---------8---------9--------10
             return @"Sub command:
+  server       Syslog待ち受けモード
+               以下の接続が使用可能
+               - UDP
+               - TCP (暗号化無し)
+               - TCP (暗号化有り, クライアント証明書無し)
+               - TCP (暗号化有り, クライアント証明書有り)
+  client       Syslog送信
 ";
-            
-            string text1 = "Sub command:" + "\r\n";
-            string text2 = "    server       Syslog待ち受けモード。" + "\r\n" +
-                           "                 UDP/TCP/TCP(暗号化,クライアント証明書無し)/TCP(暗号化,クライアント証明書有り)";
-            string text3 = "    client       Syslog送信。";
-
-            return text1 + "\r\n" + text2 + "\r\n" + text3 + "\r\n";
         }
 
         public static string HelpBlock4()
         {
             //  Options
-            //             ---------1---------2---------3---------4---------5---------6---------7---------8---------9--------10
-
-            return null;
+            //  ---2---------3---------4---------5---------6---------7---------8---------9--------10
+            return @"Options:
+    server options:
+    /u         UDP待ち受け。接続許可するネットワークアドレスと待ち受けポートを指定。
+               使用例) 192.168.24.0:514 ⇒ 
+";
         }
     }
 }
